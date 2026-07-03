7c604174194fe325a6ea3872db02c531ac17677a
Sudhanshu-Singh-Parmar <sudhanshuparmar95@gmail.com>
2026-07-03 15:29:44 +0530

    implement update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,17 +13,50 @@ export const getUser = async (req, res, next) => {
         }
         console.log('user data fetch sucessfully');
         user.password = undefined;
-        return res.status(500).json({
+        return res.status(200).json({
             success: true,
             message: 'user data fetch sucessfully',
             user
         })
-
+        
     } catch (error) {
         console.log('error in getUser')
         return res.status(500).json({
             success: false,
-            message: 'error in getUser'
+            message: 'error in getUser',
+            error
         })
+    }
+}
+
+export const updateUser = async (req, res, next) => {
+    try {
+        const userId = req.body.id;
+        
+        // now update
+        const { userName, phone, address } = req.body;
+        const updatedUser = await User.findByIdAndUpdate(userId, {userName, phone, address}, {new: true, runValidators: true});
+
+        if(!updatedUser) {
+            console.log('user not found, most probably user got deleted just before sending updatedUser data:', error)
+            return res.status(500).json({
+                success: false,
+                message: 'user not found',
+            })
+        }
+        
+        updatedUser.password = undefined
+        return res.status(200).json({
+            success: true,
+            message: 'user updated sucessfully',
+            updatedUser
+        })
+    } catch (error) {
+        console.log('error in updateUser Route', error)
+        return res.status(500).json({
+            success: false,
+            message: 'error in updateUser Route',
+        })
+        
     }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getUser } from '../controllers/userController.js';
+import { getUser, updateUser } from '../controllers/userController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 
 const userRoutes = express.Router();
 
 userRoutes.get('/getUser', authMiddleware, getUser);
+userRoutes.post('/updateUser', authMiddleware, updateUser);
 
 export default userRoutes;
