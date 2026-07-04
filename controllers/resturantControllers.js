963586a73d399d262745d642532121040162d778
Sudhanshu-Singh-Parmar <sudhanshuparmar95@gmail.com>
2026-07-04 16:55:22 +0530

    implement get resturant by id

--- controllers/resturantControllers.js
+++ controllers/resturantControllers.js
@@ -24,5 +24,45 @@ export const createResturant = async (req, res, next) => {
             message: "error in create resturant API"
         })
     }
-    
+}
+
+export const getAllResturants = async (req,res, next) => {
+    try {
+        const resturants = await Resturant.find();
+        res.status(200).json({
+            success: true,
+            message: "All resturants fetched successfully",
+            resturants
+        })
+    } catch (error) {
+        console.log('error in get all resturants API:', error)
+        return res.status(500).json({
+            success: false,
+            message: "error in get all resturants API"
+        })
+    }
+}
+
+export const getResturantById = async (req,res, next) => {
+    try {
+        const resturantId = req.params.id;
+        const resturant = await Resturant.findById(resturantId);
+        if(!resturant) {
+            return res.status(404).json({
+                success: false,
+                message: "no resturant found"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "resturant fetched successfully",
+            resturant
+        })
+    } catch (error) {
+        console.log('error in get resturant API:', error)
+        return res.status(500).json({
+            success: false,
+            message: "error in get resturant API"
+        })
+    }
 }
--- routes/resturantRoutes.js
+++ routes/resturantRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { createResturant } from '../controllers/resturantControllers.js';
+import { createResturant, getAllResturants, getResturantById } from '../controllers/resturantControllers.js';
 
 
 const resturantRoutes = express.Router();
 
 resturantRoutes.post('/create', authMiddleware, createResturant);
+resturantRoutes.get('/get-all-resturants', getAllResturants);
+resturantRoutes.get('/get-resturant/:id', getResturantById);
 
 export default resturantRoutes;
