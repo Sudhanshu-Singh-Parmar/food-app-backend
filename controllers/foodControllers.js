425934d3b70d92abe92797c0aae242218e1285b7
Sudhanshu-Singh-Parmar <sudhanshuparmar95@gmail.com>
2026-07-07 15:53:43 +0530

    implement get food by resturant

--- controllers/foodControllers.js
+++ controllers/foodControllers.js
@@ -66,3 +66,28 @@ export const getFoodById = async (req, res, next) => {
         })
     }
 }
+
+export const getFoodByResturant = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const food = await Food.find({resturant: id});
+        if(!food) {
+            return res.status(404).json({
+                success: false,
+                message: 'food based on resturant not found'
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: 'food based on resturant fetched successfully',
+            food
+        })
+        
+    } catch (error) {
+        console.log('error in get food API:', error);
+        return res.status(500).json({
+            success: false,
+            message: 'error in get food API'
+        })
+    }
+}
--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { createFood, getAllFoods, getFoodById } from '../controllers/foodControllers.js';
+import { createFood, getAllFoods, getFoodById, getFoodByResturant } from '../controllers/foodControllers.js';
 
 const foodRoutes = express.Router();
 
 foodRoutes.post('/create-food', authMiddleware, createFood);
 foodRoutes.get('/get-all-foods', getAllFoods );
 foodRoutes.get('/get-food/:id', getFoodById );
+foodRoutes.get('/get-food-by-resturant/:id', getFoodByResturant);
 
 export default foodRoutes;
